7602cfc54aef271259b43705112941b1ca19e81e
gonzalo <gonzalo_a_fuente@hotmail.com>
2022-01-14 18:15:37 -0300

    Initial commit - Sample Web page created

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0E9594',
+        secondary: {
+          100: '#F5DFBB',
+          200: '#562C2C ',
+        }
+      },
+      fontFamily: {
+        body: ['Titillium Web']
+      }
+    },
+  },
+  plugins: [],
+}
